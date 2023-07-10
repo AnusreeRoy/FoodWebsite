e668cf34eb2bd6da155b290d453b3bc333d62c29
AnusreeRoy <119887004+AnusreeRoy@users.noreply.github.com>
2023-07-10 21:57:42 +0600

    update

--- nextjs-project/components/Footer.js
+++ nextjs-project/components/Footer.js
@@ -1,12 +1,24 @@
 import styles from "../styles/test.module.css";
 import Link from "next/link";
 import React from 'react'
-
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
+import{
+  faTwitter,
+  faInstragram,
+  faFacebook
+}from '@fortawesome/free-solid-svg-icons'
+library.add(
+  faTwitter,
+  faInstragram,
+  faFacebook
+)
 function Footer() {
   return (
     <div>
         <ul>
-            <li><Link href="/">Facebook</Link></li>
+            <li><Link href="/"><FontAwesomeIcon icon={facode} style={{width:'40px'}}/></Link></li>
                <li> <Link href="/">Instagram</Link></li>
                <li> <Link href="/">twitter</Link></li>
              </ul>
