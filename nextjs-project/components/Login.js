347da0fdff2cc03e52d8e5a4cec53746363e0f84
AnusreeRoy <119887004+AnusreeRoy@users.noreply.github.com>
2023-07-16 01:21:07 +0600

    update

--- nextjs-project/components/Login.js
+++ nextjs-project/components/Login.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import styles from "../styles/test.module.css";
+import Link from 'next/link';
 function Login() {
   return (
-    <div className={styles.loginpg}>
  <div className={styles.box}>
 <div className={styles.boximg}>
+<h1 className={styles.loginh1}>DeliEats</h1>
   </div>
   <div className={styles.input}>
     <label for="Email">Email</label>
     <input type="text" placeholder="Email Address"/>
     <label for="password">Password</label>
-    <input type="text" placeholder="password"/>
+    <input type="password" placeholder="password"/>
 </div>
 <div className={styles.button}>
-  <button>Login</button>
+  <button><Link href="/"></Link>Login</button>
+</div>
+<div className={styles.signuplink}>
+  <p>Don't Have an account yet? <Link href="/signup">Signup</Link></p>
 </div>
  </div>
-    </div>
+   
   )
 }
 
--- nextjs-project/pages/login.js
+++ nextjs-project/pages/login.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Login from '@/components/Login'
+import styles from "../styles/test.module.css"
+import Head from "next/head";
 function login() {
   return (
-    <div>
+    <>
+    <Head> 
+      <title>DeliEats: Login</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="description"content="Food Website Login Page"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    </Head>
+    <div className={styles.loginpg}>
       <Login/>  
     </div>
+    </>
+    
   )
 }
 
