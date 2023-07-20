82b69bd91dc859fce15621a4b5f787808034780e
AnusreeRoy <119887004+AnusreeRoy@users.noreply.github.com>
2023-07-20 23:31:27 +0600

    up

--- nextjs-project/pages/profile.js
+++ nextjs-project/pages/profile.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
 import styles from "../styles/test.module.css";
 import Nav2 from '@/components/Nav2';
+import Head from 'next/head';
 function profile() {
   
   return (
     <div>
+      <Head> 
+      <title>DeliEats: Profile</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="description"content="Food Website Profile Page"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    </Head>
         <div className={styles.head}>
         <header className={styles.header}>
           <h1 className={styles.h1}>DeliEats</h1>    
