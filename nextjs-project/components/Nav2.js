820c7c8384c216b74858d8dd2acbda5f66d03dca
AnusreeRoy <119887004+AnusreeRoy@users.noreply.github.com>
2023-07-20 23:28:32 +0600

    update

--- nextjs-project/components/Nav2.js
+++ nextjs-project/components/Nav2.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import Link from 'next/link';
 import styles from "../styles/test.module.css";
 import { BiSolidUser, BiToggleLeft } from "react-icons/bi";
 
 function Nav2() {
+  const [open, setOpen]= useState(false);
+  let menuRef=useRef();
+  useEffect(()=>{
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+        setOpen(false);
+      }
+      
+    };
+    document.addEventListener("mousedown",handler);
+    return()=>{
+      document.removeEventListener("mousedown",handler);
+    }
+  });
   return (
     <div>
         <nav className={styles.drop}>
-        <div className={styles.dropdown}>
-            <div className={styles.dropbtn}><BiSolidUser/></div>
-                <ul className={styles.dropcont}>
+        <div className={styles.dropdown} ref={menuRef}>
+            <div className={styles.dropbtn} onClick={(e)=> setOpen(!open)}><BiSolidUser/></div>
+                {open && (
+                  <ul className={styles.dropcont}>
                 <li><Link className={styles.links} href="/">Profile Info</Link></li>
                <li><Link className={styles.links}  href="/">Order History</Link></li>
                 <li><Link className={styles.links}  href="/">Vouchers</Link></li>
-                </ul>  
+                </ul>
+                )}
+                  
             </div>
             <button className={styles.logout}>
           <Link className={styles.log} href="/login" alt="">Logout</Link>
--- nextjs-project/pages/profile.js
+++ nextjs-project/pages/profile.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "../styles/test.module.css";
 import Nav2 from '@/components/Nav2';
 function profile() {
+  
   return (
     <div>
         <div className={styles.head}>
         <header className={styles.header}>
           <h1 className={styles.h1}>DeliEats</h1>    
-          <Nav2/>   
+           <Nav2/>
         </header>
       </div>
     </div>
